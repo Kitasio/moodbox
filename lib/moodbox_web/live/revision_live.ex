7445d5139aaf57016774cce4e4d3e38bd5816493
Kitasio <kit.plotits@gmail.com>
2025-01-05 14:43:45 +0100

    refactor: Update revision page to show 10 links dynamically

--- lib/moodbox_web/live/revision_live.ex
+++ lib/moodbox_web/live/revision_live.ex
@@ -26,20 +26,13 @@ defmodule MoodboxWeb.RevisionLive do
         </.subheading>
         <div class="mt-10 lg:mt-20 flex gap-5 xl:gap-7 flex-wrap justify-center">
           <.link
-            :for={i <- 1..9}
+            :for={i <- 1..10}
             patch={
               ~p"/moods/#{@mood}/#{@intensity}/#{@texture}/#{@location}/#{@description}/outcome/classical/revision/#{i}"
             }
           >
             <div class="flex items-center justify-center text-xs sm:text-sm lg:text-xl font-semibold w-10 h-10 lg:w-20 lg:h-20 xl:w-24 xl:h-24 shadow-md hover:scale-105 transition rounded-full bg-[#6b2a6d]  text-white text-sm">
               <%= i %>
-            </div>
-          </.link>
-          <.link patch={
-            ~p"/moods/#{@mood}/#{@intensity}/#{@texture}/#{@location}/#{@description}/outcome/classical/revision/deep-dive"
-          }>
-            <div class="flex items-center justify-center text-xs sm:text-sm lg:text-xl font-semibold w-10 h-10 lg:w-20 lg:h-20 xl:w-24 xl:h-24 shadow-md hover:scale-105 transition rounded-full bg-[#6b2a6d]  text-white text-sm">
-              10
             </div>
           </.link>
         </div>
