f7583156770efcd970070bce979c094d6fb21483
Your Name <you@example.com>
2025-04-25 15:19:40 +0200

    Add ritual images

--- lib/moodbox_web/live/create_ritual.ex
+++ lib/moodbox_web/live/create_ritual.ex
@@ -26,12 +26,12 @@ defmodule MoodboxWeb.CreateRitualLive do
           <.p>Here Are More Ideas You Can Add To Deepen And Expand Your Ritual.</.p>
 
           <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 mt-8 w-full max-w-4xl">
-            <.ritual_card title="POWER POSES & FLOW" description="Yoga & Power Posing" link_href="/power-poses" current_path={@current_path} />
-            <.ritual_card title="CRYSTAL FREQUENCIES" description="Crystal Image Download" link_href="/crystal-frequencies" current_path={@current_path} />
-            <.ritual_card title="ELEMENTAL IMMERSION" description="Bathtime Rituals-Salts & Candles Affiliate AJ" link_href="/elemental-immersion" current_path={@current_path} />
-            <.ritual_card title="ALCHEMY OF SCENT" description="Aromatherapy Blends-Affiliate Doterra" link_href="/alchemy-of-scent" current_path={@current_path} />
-            <.ritual_card title="TEA CEREMONY" description="Herbal Tea Blends" link_href="/tea-ceremony" current_path={@current_path} />
-            <.ritual_card title="INTENTION INFUSION" description="Intentional Clothing, Coffee Mugs Etc" link_href="/intention-infusion" current_path={@current_path} />
+            <.ritual_card title="POWER POSES & FLOW" description="Yoga & Power Posing" link_href="/power-poses" current_path={@current_path} img_url="https://ik.imagekit.io/soulgenesis/Moodinabox/ritual_monk.png?updatedAt=1745586775656" />
+            <.ritual_card title="CRYSTAL FREQUENCIES" description="Crystal Image Download" link_href="/crystal-frequencies" current_path={@current_path} img_url="https://ik.imagekit.io/soulgenesis/Moodinabox/ritual_crystal.png?updatedAt=1745586728866" />
+            <.ritual_card title="ELEMENTAL IMMERSION" description="Bathtime Rituals-Salts & Candles Affiliate AJ" link_href="/elemental-immersion" current_path={@current_path} img_url="https://ik.imagekit.io/soulgenesis/Moodinabox/ritual_candle.png?updatedAt=1745586704900" />
+            <.ritual_card title="ALCHEMY OF SCENT" description="Aromatherapy Blends-Affiliate Doterra" link_href="/alchemy-of-scent" current_path={@current_path} img_url="https://ik.imagekit.io/soulgenesis/Moodinabox/ritual_diffuser.png?updatedAt=1745586760326" />
+            <.ritual_card title="TEA CEREMONY" description="Herbal Tea Blends" link_href="/tea-ceremony" current_path={@current_path} img_url="https://ik.imagekit.io/soulgenesis/Moodinabox/ritual_tea.png?updatedAt=1745586281177" />
+            <.ritual_card title="INTENTION INFUSION" description="Intentional Clothing, Coffee Mugs Etc" link_href="/intention-infusion" current_path={@current_path} img_url="" />
           </div>
 
           <.p class="mt-10 max-w-3xl">
@@ -51,13 +51,12 @@ defmodule MoodboxWeb.CreateRitualLive do
   attr :description, :string, required: true
   attr :link_href, :string, required: true
   attr :current_path, :string, required: true
+  attr :img_url, :string, required: true
 
   defp ritual_card(assigns) do
     ~H"""
     <.link patch={@current_path <> @link_href} class="flex flex-col items-center gap-4 group">
-      <div class="w-40 h-40 border border-gray-400 rounded-3xl bg-white bg-opacity-20 group-hover:scale-105 transition-transform duration-200">
-        <%!-- Placeholder for image --%>
-      </div>
+      <img src={@img_url} class="w-40 h-40 rounded-3xl bg-white bg-opacity-20 group-hover:scale-105 transition-transform duration-200" />
       <div class="bg-[#6b2a6d] text-white px-4 py-2 rounded-full text-sm font-semibold transition-colors duration-200">
         <%= @title %>
       </div>
