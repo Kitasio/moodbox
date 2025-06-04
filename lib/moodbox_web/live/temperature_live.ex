946d84e72d62f9942e5a2d1602b4a350a95ad4e4
Your Name <you@example.com>
2025-06-04 14:27:51 +0200

    Update subheading text

--- lib/moodbox_web/live/temperature_live.ex
+++ lib/moodbox_web/live/temperature_live.ex
@@ -14,34 +14,20 @@ defmodule MoodboxWeb.TemperatureLive do
     <.container class="overflow-hidden">
       <.centered_block>
         <.subheading>
-          If your feeling had a temperature, what would it be?
+          The states of our emotions can be compared to water which can be frozen, like ice, liquid…
         </.subheading>
 
         <div class="mt-4 text-center">
           <button phx-click={show_modal("temperature-modal")} class="">
-            <div class="flex gap-1 items-center text-lg font-semibold">
-              More Info
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="w-4 h-4"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z"
-                />
-              </svg>
+            <div class="flex gap-1 items-center text-lg cursor-pointer hover:underline">
+              Read more
             </div>
           </button>
         </div>
 
         <.modal id="temperature-modal" on_cancel={hide_modal("temperature-modal")}>
           <p class="text-lg font-medium leading-relaxed">
-            Our emotions have a temperature. The states of our emotions can be compared to water, which can be frozen like ice, liquid and flowing or steamy like vapor. Rage, panic or grief are examples of hot. Resentment, paralysis or resignation are examples of cold.
+            The states of our emotions can be compared to water, which can be frozen like ice, liquid and flowing or steamy like vapor. Rage, panic or grief are examples of hot. Resentment, paralysis or resignation are examples of cold.
           </p>
 
           <p class="mt-3 text-lg font-medium leading-relaxed">
