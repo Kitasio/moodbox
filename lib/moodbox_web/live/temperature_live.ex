eef55d5e5c8e13b00d054a5e2b7c8099f6c0a09c
Your Name <you@example.com>
2025-08-21 12:01:55 +0200

    Remove read more modal

--- lib/moodbox_web/live/temperature_live.ex
+++ lib/moodbox_web/live/temperature_live.ex
@@ -11,60 +11,50 @@ defmodule MoodboxWeb.TemperatureLive do
 
   def render(assigns) do
     ~H"""
-    <.container class="overflow-hidden">
-      <.centered_block>
-        <.subheading>
-          The states of our emotions can be compared to water which can be frozen, like ice, liquid…
-        </.subheading>
+    <.container>
+      <div class="h-dvh overflow-y-scroll">
+        <.centered_block>
+          <.subheading>
+            Emotions Flow Like Water
+          </.subheading>
 
-        <div class="mt-4 text-center">
-          <button phx-click={show_modal("temperature-modal")} class="">
-            <div class="flex gap-1 items-center text-lg cursor-pointer hover:underline">
-              Read more
-            </div>
-          </button>
-        </div>
-
-        <.modal id="temperature-modal" on_cancel={hide_modal("temperature-modal")}>
-          <p class="text-lg font-medium leading-relaxed">
+          <.p class="mt-8 text-left p-4 md:p-8 bg-white rounded-xl shadow-md">
             The states of our emotions can be compared to water, which can be frozen like ice, liquid and flowing or steamy like vapor. Rage, panic or grief are examples of hot. Resentment, paralysis or resignation are examples of cold.
-          </p>
-
-          <p class="mt-3 text-lg font-medium leading-relaxed">
+            <br /><br />
             Understanding what state your emotion is in allows you to heat it up or cool it off so it can flow through and be released as it’s intended to. Emotions aren’t meant to stay with us. They give us a message so that we can change course and find equilibrium.
-          </p>
-        </.modal>
+          </.p>
 
-        <div class="mt-10 lg:mt-20">
-          <.link
-            :for={temp <- @temperatures}
-            patch={@current_path <> "/#{temp.resource}"}
-            class="mt-10 flex w-44 sm:w-96"
-          >
-            <.btn class="transition hover:scale-105">
-              <%= temp.name %>
-            </.btn>
-          </.link>
-        </div>
-      </.centered_block>
+          <div class="mt-8 lg:mt-12">
+            <.link
+              :for={temp <- @temperatures}
+              patch={@current_path <> "/#{temp.resource}"}
+              class="mt-10 flex w-44 sm:w-96"
+            >
+              <.btn class="transition hover:scale-105">
+                <%= temp.name %>
+              </.btn>
+            </.link>
+          </div>
+        </.centered_block>
 
-      <.base_bg />
+        <.base_bg />
 
-      <img
-        phx-mounted={
-          JS.transition({"ease-out duration-1000", "translate-y-[50%]", "translate-y-[35%]"})
-        }
-        class="absolute translate-y-[50%] z-10 bottom-0 sm:right-10 right-4 w-32 lg:w-56 transform transition-transform"
-        src="https://moodbox.fly.storage.tigris.dev/right-flower.webp"
-      />
+        <img
+          phx-mounted={
+            JS.transition({"ease-out duration-1000", "translate-y-[50%]", "translate-y-[35%]"})
+          }
+          class="fixed translate-y-[50%] z-10 bottom-0 sm:right-10 right-4 w-32 lg:w-56 transform transition-transform"
+          src="https://moodbox.fly.storage.tigris.dev/right-flower.webp"
+        />
 
-      <img
-        phx-mounted={
-          JS.transition({"ease-out duration-1000", "translate-y-[50%]", "translate-y-[35%]"})
-        }
-        class="absolute translate-y-[50%] z-10 bottom-0 left-4 sm:left-10 w-32 lg:w-56 transform transition-transform"
-        src="https://moodbox.fly.storage.tigris.dev/left-flower.webp"
-      />
+        <img
+          phx-mounted={
+            JS.transition({"ease-out duration-1000", "translate-y-[50%]", "translate-y-[35%]"})
+          }
+          class="fixed translate-y-[50%] z-10 bottom-0 left-4 sm:left-10 w-32 lg:w-56 transform transition-transform"
+          src="https://moodbox.fly.storage.tigris.dev/left-flower.webp"
+        />
+      </div>
     </.container>
     """
   end
