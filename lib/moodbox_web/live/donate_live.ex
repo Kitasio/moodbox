15748ffed1ec39cf92c8e47a2ec7fc9ee69f8334
Your Name <you@example.com>
2025-08-31 15:07:34 +0200

    Update padding

--- lib/moodbox_web/live/donate_live.ex
+++ lib/moodbox_web/live/donate_live.ex
@@ -7,7 +7,7 @@ defmodule MoodboxWeb.DonateLive do
       <.centered_block>
         <.heading>Support the Shift: Keep the Vibes Flowing</.heading>
         <.subheading>When you give more, you get more.</.subheading>
-        <.p class="text-left mt-4 md:mt-8 px-4 md:px-8 bg-white rounded-xl shadow-md whitespace-pre-line">
+        <.p class="text-left mt-4 md:mt-8 pb-4 md:pb-8 px-4 md:px-8 bg-white rounded-xl shadow-md whitespace-pre-line">
           Mood in a Box is designed to be free and accessible to anyone, anywhere—because we believe that good moods are contagious, creative, and catalytic. When even one person shifts into a better mood, it sparks a ripple effect: brighter thoughts, kinder choices, and more inspired action. Enough of those can light up the whole planet.
 
           If this experience has sparked even the slightest lightness or clarity—for you or someone you’re sharing it with—please consider making a donation.
